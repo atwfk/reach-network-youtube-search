aba3ffbffe17fb28c76797b6d475db928a7e00de
atwfk <ahmed.twfiek8@gmail.com>
2022-02-27 16:18:31 +0200

    add snapshot tersting lib

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
 import App from "./App";
 
-test("renders learn react link", () => {
+it("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it("renders correctly when there are no items", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
