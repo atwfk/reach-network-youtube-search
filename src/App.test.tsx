1a215af7ed33ea76206f01f25b1182d6734402b8
atwfk <ahmed.twfiek8@gmail.com>
2022-02-27 16:20:10 +0200

    remove App compopnent snapshots

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import renderer from "react-test-renderer";
 
 import App from "./App";
 
@@ -7,9 +6,4 @@ it("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
-
-it("renders correctly when there are no items", () => {
-  const tree = renderer.create(<App />).toJSON();
-  expect(tree).toMatchSnapshot();
 });
