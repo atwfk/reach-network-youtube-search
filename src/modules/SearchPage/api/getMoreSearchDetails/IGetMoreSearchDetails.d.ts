0585d94677fafcb4f6d4af5dc60399a924607b93
atwfk <ahmed.twfiek8@gmail.com>
2022-03-01 01:04:07 +0200

    created getMoreSearchDetails function to fetch search list details

---
+++ src/modules/SearchPage/api/getMoreSearchDetails/IGetMoreSearchDetails.d.ts
@@ -0,0 +1,75 @@
+declare namespace IGetMoreSearchDetails {
+  export interface IVideoDetailsApi {
+    kind: "youtube#video";
+    etag: string;
+    id: string;
+    contentDetails: {
+      duration: string;
+      dimension: "2d" | "3d";
+      definition: "hd" | "sd";
+      caption: boolean;
+      licensedContent: boolean;
+      contentRating: Record<string, string>;
+      projection: string;
+    };
+    statistics: {
+      viewCount: string;
+      likeCount: string;
+      favoriteCount: string;
+      commentCount: string;
+    };
+  }
+
+  export interface IChannelDetailsApi {
+    kind: "youtube#channel";
+    etag: string;
+    id: string;
+    statistics: {
+      viewCount: string;
+      subscriberCount: string;
+      hiddenSubscriberCount: boolean;
+      videoCount: string;
+    };
+  }
+
+  export interface IPlayListDetailsApi {
+    kind: "youtube#playlist";
+    etag: string;
+    id: string;
+    contentDetails: {
+      itemCount: number;
+    };
+  }
+
+  export interface IVideoResApi {
+    kind: "youtube#videoListResponse";
+    etag: string;
+    pageInfo: {
+      totalResults: number;
+      resultsPerPage: number;
+    };
+    items: IVideoDetailsApi[];
+  }
+
+  export interface IChannelResApi {
+    kind: "youtube#channelListResponse";
+    etag: string;
+    pageInfo: {
+      totalResults: number;
+      resultsPerPage: number;
+    };
+    items: IChannelDetailsApi[];
+  }
+
+  export interface IPlayListResApi {
+    kind: "youtube#playlistListResponse";
+    etag: string;
+    pageInfo: {
+      totalResults: number;
+      resultsPerPage: number;
+    };
+    items: IPlayListDetailsApi[];
+  }
+}
+
+export { IGetMoreSearchDetails };
---
+++ src/modules/SearchPage/api/getMoreSearchDetails/getMoreSearchDetails.ts
@@ -0,0 +1,43 @@
+import { API_KEY } from "../../../shared/api";
+import { getApis } from "../../../shared/api/network";
+import { IError } from "../../../shared/types/IError";
+import { IGetMoreSearchDetails } from "./IGetMoreSearchDetails";
+
+export type ApisRes = [
+  IGetMoreSearchDetails.IVideoResApi,
+  IGetMoreSearchDetails.IChannelResApi,
+  IGetMoreSearchDetails.IPlayListResApi
+];
+
+type GetMoreSearchDetails = {
+  videosId: string;
+  channelsId: string;
+  playlistsId: string;
+};
+
+export const getMoreSearchDetails = async ({
+  videosId,
+  channelsId,
+  playlistsId
+}: GetMoreSearchDetails): Promise<ApisRes | IError.IErrorData> => {
+  const endPoints = [
+    `/videos?part=contentDetails, statistics&key=${API_KEY}&id=${videosId}`,
+    `/channels?part=statistics&key=${API_KEY}&id=${channelsId}`,
+    `/playlists?part=contentDetails&key=${API_KEY}&id=${playlistsId}`
+  ];
+
+  return getApis<ApisRes, ApisRes, IError.IErrorData>(
+    endPoints,
+    // @ts-expect-error Type 'ApisRes[]' is not assignable to type 'ApisRes'
+    (responses) => {
+      const data = responses.map((response) => response.data);
+
+      return data;
+    },
+    (err) => {
+      const { message, errorCode, isError, errors } = err;
+
+      throw Object.assign(new Error(), { isError, message, errorCode, errors });
+    }
+  );
+};
