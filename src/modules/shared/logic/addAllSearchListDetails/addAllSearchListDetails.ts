f3eda4da02ec886bd019f649d311d6baed836650
atwfk <ahmed.twfiek8@gmail.com>
2022-03-01 01:07:13 +0200

    created searchListDetails transformation functions to manipulate search snippets with search details

---
+++ src/modules/shared/logic/addAllSearchListDetails/addAllSearchListDetails.ts
@@ -0,0 +1,91 @@
+import { IGetMoreSearchDetails } from "../../../SearchPage/api/getMoreSearchDetails/IGetMoreSearchDetails";
+import { IGetSearchRes } from "../../../SearchPage/api/getSearchRes/IGetSearchRes";
+import { IData } from "../../types/searchData/IData";
+import { IVideo } from "../../types/searchData/IVideo";
+import { IChannel } from "../../types/searchData/IChannel";
+import { IPlaylist } from "../../types/searchData/IPlaylist";
+
+type TransformSearchListDetails = {
+  videos: IGetMoreSearchDetails.IVideoResApi;
+  channels: IGetMoreSearchDetails.IChannelResApi;
+  playlists: IGetMoreSearchDetails.IPlayListResApi;
+};
+
+type AddAllSearchListDetails = {
+  searchData: IGetSearchRes.IResApi;
+  searchListDetails: Map<
+    string,
+    | IGetMoreSearchDetails.IVideoDetailsApi
+    | IGetMoreSearchDetails.IChannelDetailsApi
+    | IGetMoreSearchDetails.IPlayListDetailsApi
+  >;
+};
+
+export const transformSearchListDetails = ({
+  videos,
+  channels,
+  playlists
+}: TransformSearchListDetails): Map<
+  string,
+  | IGetMoreSearchDetails.IVideoDetailsApi
+  | IGetMoreSearchDetails.IChannelDetailsApi
+  | IGetMoreSearchDetails.IPlayListDetailsApi
+> => {
+  const searchListDetails = [...videos.items, ...channels.items, ...playlists.items];
+
+  const mapSearchList = new Map();
+
+  searchListDetails.forEach((searchItemDetails) => {
+    mapSearchList.set(searchItemDetails.id, searchItemDetails);
+  });
+
+  return mapSearchList;
+};
+
+export const addAllSearchListDetails = ({
+  searchData,
+  searchListDetails
+}: AddAllSearchListDetails): IData.IMainData => {
+  const searchListDetailsRes = searchData.items.map((searchItem) => {
+    if (searchItem.id.videoId) {
+      const videoDetails = searchListDetails.get(
+        searchItem.id.videoId
+      ) as IGetMoreSearchDetails.IVideoDetailsApi;
+
+      return {
+        ...searchItem,
+        contentDetails: videoDetails.contentDetails,
+        statistics: videoDetails.statistics
+      };
+    }
+
+    if (searchItem.id.channelId) {
+      const channelDetails = searchListDetails.get(
+        searchItem.id.channelId
+      ) as IGetMoreSearchDetails.IChannelDetailsApi;
+
+      return {
+        ...searchItem,
+        statistics: channelDetails.statistics
+      };
+    }
+
+    if (searchItem.id.playlistId) {
+      const playlistDetails = searchListDetails.get(
+        searchItem.id.playlistId
+      ) as IGetMoreSearchDetails.IPlayListDetailsApi;
+
+      return {
+        ...searchItem,
+        contentDetails: playlistDetails.contentDetails
+      };
+    }
+
+    return;
+  });
+
+  return {
+    ...searchData,
+    items: searchListDetailsRes as IVideo[] | IPlaylist[] | IChannel[]
+  };
+};
