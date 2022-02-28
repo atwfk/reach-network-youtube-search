02a000569293d16d5be816af9cb4a3cf8acd6bba
atwfk <ahmed.twfiek8@gmail.com>
2022-03-01 01:07:41 +0200

    created getAllSearchList api function

---
+++ src/modules/SearchPage/api/getAllSearchList/getAllSearchList.ts
@@ -0,0 +1,45 @@
+import { IError } from "./../../../shared/types/IError";
+import {
+  addAllSearchListDetails,
+  transformSearchListDetails
+} from "./../../../shared/logic/addAllSearchListDetails/addAllSearchListDetails";
+import { ApisRes, getMoreSearchDetails } from "./../getMoreSearchDetails/getMoreSearchDetails";
+import { transformSearchListIds } from "./../../../shared/logic/transformSearchListIds/transformSearchListIds";
+import { getSearchRes } from "../getSearchRes/getSearchRes";
+import { IGetSearchRes } from "../getSearchRes/IGetSearchRes";
+import { IData } from "../../../shared/types/searchData/IData";
+
+export const getAllSearchList = async ({
+  query
+}: {
+  query: string;
+}): Promise<IData.IMainData | IError.IErrorData> => {
+  try {
+    const searchSnippets = (await getSearchRes({ query })) as IGetSearchRes.IResApi;
+
+    const [videosId, channelsId, playlistsId] = transformSearchListIds(searchSnippets.items);
+
+    const searchListDetails = (await getMoreSearchDetails({
+      channelsId,
+      videosId,
+      playlistsId
+    })) as ApisRes;
+
+    const transformedSearchListDetails = transformSearchListDetails({
+      videos: searchListDetails[0],
+      channels: searchListDetails[1],
+      playlists: searchListDetails[2]
+    });
+
+    const searchListDetailsRes = addAllSearchListDetails({
+      searchData: searchSnippets,
+      searchListDetails: transformedSearchListDetails
+    });
+
+    return searchListDetailsRes;
+  } catch (err: unknown) {
+    const { message, errorCode, isError, errors } = err as IError.IErrorData;
+
+    throw Object.assign(new Error(), { isError, message, errorCode, errors });
+  }
+};
