a78fc531c6f3d14bd64cdd50d6a08ec318d96646
atwfk <ahmed.twfiek8@gmail.com>
2022-03-01 01:02:24 +0200

    created getApi and getApis generic functions

---
+++ src/modules/shared/api/network.ts
@@ -0,0 +1,74 @@
+import { api } from ".";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import { IError } from "../types/IError";
+
+export const getApi = async <T, X, Y>(
+  endPoint: string,
+  onSuccess: (successData: AxiosResponse<T>) => X,
+  onError: (errorData: IError.IErrorData) => Y,
+  onFinished?: () => void
+): Promise<X | Y> => {
+  try {
+    const response = await api.get(endPoint);
+
+    return onSuccess(response);
+  } catch (err: unknown) {
+    const { response } = err as IError.IErrorResponse;
+
+    if (!response) {
+      return onError({
+        isError: true,
+        message: "network error",
+        errors: null,
+        errorCode: 500
+      });
+    }
+
+    const { errors, code } = response.data.error;
+
+    return onError({
+      isError: true,
+      errors: errors[0],
+      message: errors[0].message,
+      errorCode: code
+    });
+  } finally {
+    onFinished?.();
+  }
+};
+
+export const getApis = async <T, X, Y>(
+  endPoints: string[],
+  onSuccess: (successData: AxiosResponse<T>[]) => X,
+  onError: (errorData: IError.IErrorData) => Y,
+  onFinished?: () => void
+): Promise<X | Y> => {
+  try {
+    const responses = await axios.all(endPoints.map((endPoint) => api.get(endPoint)));
+
+    return onSuccess(responses);
+  } catch (err: unknown) {
+    const { response } = err as IError.IErrorResponse;
+
+    if (!response) {
+      return onError({
+        isError: true,
+        message: "network error",
+        errors: null,
+        errorCode: 500
+      });
+    }
+
+    const { errors, code } = response.data.error;
+
+    return onError({
+      isError: true,
+      errors: errors[0],
+      message: errors[0].message,
+      errorCode: code
+    });
+  } finally {
+    onFinished?.();
+  }
+};
