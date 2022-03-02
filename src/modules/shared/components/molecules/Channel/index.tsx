cce59fa675bc60edfba7d1eb35139e2ec04d3eaa
atwfk <ahmed.twfiek8@gmail.com>
2022-03-02 02:27:02 +0200

    created Channel component

---
+++ src/modules/shared/components/molecules/Channel/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import type { FC, ReactElement } from "react";
+import ItemDetails from "../ItemDetails";
+import { transformIntNum } from "../../../logic/transformIntNum/transformIntNum";
+import ImageLayer from "../ImageLayer";
+import { useMedia } from "../../../hooks/useMedia";
+import { SCREENS } from "../../../constants";
+import { IChannel } from "../../../types/searchData/IChannel";
+
+const Channel: FC<{ channel: IChannel }> = ({ channel }): ReactElement => {
+  const matched = useMedia(`(max-width: ${SCREENS.SM})`);
+
+  const imageWidth = matched ? 120 : 320;
+  const imageHeight = matched ? 90 : 180;
+
+  const stats = [
+    `${transformIntNum(channel.statistics.subscriberCount)} subscribers`,
+    `${transformIntNum(channel.statistics.videoCount)} videos`
+  ];
+
+  return (
+    <li className="search-item">
+      <ImageLayer
+        alt={channel.snippet.title}
+        width={`${imageWidth}`}
+        height={`${imageHeight}`}
+        src={channel.snippet.thumbnails.medium.url}
+      />
+      <ItemDetails
+        description={channel.snippet.description}
+        stats={stats}
+        title={channel.snippet.title}
+      />
+    </li>
+  );
+};
+
+export default Channel;
