e709053bf7f2da448038533803f9084cb135e25b
atwfk <ahmed.twfiek8@gmail.com>
2022-03-02 02:15:01 +0200

    add COLORS constan

--- src/modules/shared/constants.ts
+++ src/modules/shared/constants.ts
@@ -1,8 +1,12 @@
-export const VIDEOS_LIMITS = 8;
+export const VIDEOS_LIMITS = 30;
 
 export const SCREENS = {
   SM: "640px",
   MD: "768px",
   LG: "920px",
   XL: "1280px"
+};
+
+export const COLORS = {
+  GREY_SHD1: "#767272"
 };
