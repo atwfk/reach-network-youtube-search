106d86df23902575d40412b2b390352d9bc9ba2a
atwfk <ahmed.twfiek8@gmail.com>
2022-03-02 02:15:43 +0200

    created Image component

---
+++ src/modules/shared/components/atoms/Image/IImage.d.ts
@@ -0,0 +1,10 @@
+declare namespace IImage {
+  export interface IProps {
+    src: string;
+    alt: string;
+    width?: string;
+    height?: string;
+  }
+}
+
+export { IImage };
---
+++ src/modules/shared/components/atoms/Image/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { FC, ReactElement } from "react";
+import { IImage } from "./IImage";
+
+const Image: FC<IImage.IProps> = ({ src, alt, width, height }): ReactElement => {
+  return <img src={src} alt={alt} width={width} height={height} />;
+};
+
+export default Image;
