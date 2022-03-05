defceb4639da00c9c4d11fa2f47bab3450c204f6
atwfk <ahmed.twfiek8@gmail.com>
2022-03-05 20:01:47 +0200

    improved Spinner css animation

--- src/modules/shared/components/atoms/Spinner/index.tsx
+++ src/modules/shared/components/atoms/Spinner/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import type { FC, ReactElement } from "react";
-import { FaSpinner } from "react-icons/fa";
 import { StyledSpinner } from "./styles";
 
 const Spinner: FC = (): ReactElement => {
   return (
     <StyledSpinner>
-      <FaSpinner className="spinner" />
+      <div className="spinner" />
     </StyledSpinner>
   );
 };
--- src/modules/shared/components/atoms/Spinner/styles.ts
+++ src/modules/shared/components/atoms/Spinner/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { COLORS } from "../../../constants";
 
 export const StyledSpinner = styled.div`
   display: flex;
@@ -6,19 +7,68 @@ export const StyledSpinner = styled.div`
   align-items: center;
 
   .spinner {
-    font-size: 50px;
-    animation: spinnerLoader 2s;
-    animation-iteration-count: infinite;
-    transition-timing-function: ease;
-    color: #767272;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    animation: loader-circles 1s linear infinite;
+    margin-top: 20px;
+    color: ${COLORS.GREY_SHD1};
   }
 
-  @keyframes spinnerLoader {
+  @keyframes loader-circles {
     0% {
-      transform: rotate(0);
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 5%), 19px -19px 0 0 rgb(0 0 0 / 10%),
+        27px 0 0 0 rgb(0 0 0 / 20%), 19px 19px 0 0 rgb(0 0 0 / 30%), 0 27px 0 0 rgb(0 0 0 / 40%),
+        -19px 19px 0 0 rgb(0 0 0 / 60%), -27px 0 0 0 rgb(0 0 0 / 80%),
+        -19px -19px 0 0 ${COLORS.GREY_SHD1};
+    }
+    12.5% {
+      box-shadow: 0 -27px 0 0 ${COLORS.GREY_SHD1}, 19px -19px 0 0 rgb(0 0 0 / 5%),
+        27px 0 0 0 rgb(0 0 0 / 10%), 19px 19px 0 0 rgb(0 0 0 / 20%), 0 27px 0 0 rgb(0 0 0 / 30%),
+        -19px 19px 0 0 rgb(0 0 0 / 40%), -27px 0 0 0 rgb(0 0 0 / 60%),
+        -19px -19px 0 0 rgb(0 0 0 / 80%);
+    }
+    25% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 80%), 19px -19px 0 0 ${COLORS.GREY_SHD1},
+        27px 0 0 0 rgb(0 0 0 / 5%), 19px 19px 0 0 rgb(0 0 0 / 10%), 0 27px 0 0 rgb(0 0 0 / 20%),
+        -19px 19px 0 0 rgb(0 0 0 / 30%), -27px 0 0 0 rgb(0 0 0 / 40%),
+        -19px -19px 0 0 rgb(0 0 0 / 60%);
+    }
+    37.5% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 60%), 19px -19px 0 0 rgb(0 0 0 / 80%),
+        27px 0 0 0 ${COLORS.GREY_SHD1}, 19px 19px 0 0 rgb(0 0 0 / 5%), 0 27px 0 0 rgb(0 0 0 / 10%),
+        -19px 19px 0 0 rgb(0 0 0 / 20%), -27px 0 0 0 rgb(0 0 0 / 30%),
+        -19px -19px 0 0 rgb(0 0 0 / 40%);
+    }
+    50% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 40%), 19px -19px 0 0 rgb(0 0 0 / 60%),
+        27px 0 0 0 rgb(0 0 0 / 80%), 19px 19px 0 0 ${COLORS.GREY_SHD1}, 0 27px 0 0 rgb(0 0 0 / 5%),
+        -19px 19px 0 0 rgb(0 0 0 / 10%), -27px 0 0 0 rgb(0 0 0 / 20%),
+        -19px -19px 0 0 rgb(0 0 0 / 30%);
+    }
+    62.5% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 30%), 19px -19px 0 0 rgb(0 0 0 / 40%),
+        27px 0 0 0 rgb(0 0 0 / 60%), 19px 19px 0 0 rgb(0 0 0 / 80%), 0 27px 0 0 ${COLORS.GREY_SHD1},
+        -19px 19px 0 0 rgb(0 0 0 / 5%), -27px 0 0 0 rgb(0 0 0 / 10%),
+        -19px -19px 0 0 rgb(0 0 0 / 20%);
+    }
+    75% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 20%), 19px -19px 0 0 rgb(0 0 0 / 30%),
+        27px 0 0 0 rgb(0 0 0 / 40%), 19px 19px 0 0 rgb(0 0 0 / 60%), 0 27px 0 0 rgb(0 0 0 / 80%),
+        -19px 19px 0 0 ${COLORS.GREY_SHD1}, -27px 0 0 0 rgb(0 0 0 / 5%),
+        -19px -19px 0 0 rgb(0 0 0 / 10%);
+    }
+    87.5% {
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 10%), 19px -19px 0 0 rgb(0 0 0 / 20%),
+        27px 0 0 0 rgb(0 0 0 / 30%), 19px 19px 0 0 rgb(0 0 0 / 40%), 0 27px 0 0 rgb(0 0 0 / 60%),
+        -19px 19px 0 0 rgb(0 0 0 / 80%), -27px 0 0 0 ${COLORS.GREY_SHD1},
+        -19px -19px 0 0 rgb(0 0 0 / 5%);
     }
     100% {
-      transform: rotate(360deg);
+      box-shadow: 0 -27px 0 0 rgb(0 0 0 / 5%), 19px -19px 0 0 rgb(0 0 0 / 10%),
+        27px 0 0 0 rgb(0 0 0 / 20%), 19px 19px 0 0 rgb(0 0 0 / 30%), 0 27px 0 0 rgb(0 0 0 / 40%),
+        -19px 19px 0 0 rgb(0 0 0 / 60%), -27px 0 0 0 rgb(0 0 0 / 80%),
+        -19px -19px 0 0 ${COLORS.GREY_SHD1};
     }
   }
 `;
