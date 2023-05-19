26bbcbe07a9fb800f42ff12c83cef272149cb8a0
atwfk <ahmed.twfiek8@gmail.com>
2023-05-19 12:32:59 +0300

    use react-ga4 event name

--- src/modules/shared/GAPixel/index.ts
+++ src/modules/shared/GAPixel/index.ts
@@ -1,39 +1,38 @@
 import ReactGA from "react-ga4";
 
 interface PixelEvent {
-  category: string;
-  action: PixelEventNames;
+  name: PixelCategory;
+  action: string;
   label?: string;
   gaOptions?: Record<string, string | number>;
 }
 
-type PixelEventNames = "clicks" | "page_view" | "search";
+type PixelCategory = "clicks" | "page_view" | "search";
 
 const logEvent = (data: PixelEvent): void => {
   for (const field in data.gaOptions) ReactGA.set({ [field]: data.gaOptions[field] });
-  ReactGA.event({
-    category: data.category.toLowerCase(),
+  ReactGA.event(data.name.toLowerCase(), {
     action: data.action.toLowerCase(),
     label: data.label
   });
 };
 
-export const GAClicks = (category: string): void =>
+export const GAClicks = (action: string): void =>
   logEvent({
-    category: category,
-    action: "clicks"
+    name: "clicks",
+    action
   });
 
-export const GAPageViews = (category: string, pageName: string): void =>
+export const GAPageViews = (action: string, pageName: string): void =>
   logEvent({
-    category: category,
-    action: "page_view",
+    name: "page_view",
+    action,
     label: pageName
   });
 
-export const GASearch = (searchValue: string, pageName: string): void =>
+export const GASearch = (action: string, pageName: string): void =>
   logEvent({
-    category: `user-searched-${searchValue}`,
-    action: "search",
+    name: "search",
+    action: `user-searched-${action}`,
     label: pageName
   });
